bbcb3b92b71754297578ab4d3fd2f91c7d07ad91
motasheron@hotmail.com <motasheron@hotmail.com@06e640d7-7332-924b-c8d3-77bc09beddf0>
2012-04-30 23:49:00 +0000

    Added snake code except for the repeating of Move Every XXX seconds or so and the controller stuff. I just realize that there is no vector class so I will have to make my own lightweight arraylist.

---
+++ DataStructure/Snake.cpp
@@ -0,0 +1,84 @@
+/*#include "Snake.h"
+Snake::Snake(int s)
+{
+	size = s;
+	cube = new Cube(s,s,s);
+	Reset();
+}
+void Snake::Reset()
+{
+	while(snake.size > 0)
+	{
+		Point* p = snake.front();
+		delete p;
+		snake.erase(p);
+	}
+	snake.push_back(new Point(1,0,0));
+	snake.push_back(new Point(2,0,0));
+	snake.push_back(new Point(3,0,0));
+}
+Point* Snake::SpawnApple()
+{
+	 Point* p = new Point(random(size),random(size),random(size));
+	 if(!CheckIfInsidePlayer(p))
+	 {
+		 delete p;
+		 return SpawnApple();
+	 }
+	 return p;
+}
+bool Snake::CheckIfInsidePlayer(Point* p)
+{
+	for(int i = 0; i <= snake.size(); i++) {
+		if(snake[i]->x == p->x && snake[i]->y == p->y && snake[i]->y == p->y)
+			return false;
+	}
+	return true;
+}
+void Snake::EatApple()
+{
+	grow = false;
+	delete apple;
+	apple = SpawnApple();
+}
+void Snake::Draw()
+{
+	cube->clear();
+	for(int i = 0; i <= snake.size(); i++) {
+		cube->setHIGH(*(snake[i]));
+	}
+	cube->setHIGH(*(apple));
+}
+void Snake::Move(int x, int y, int z)
+{
+	if(x == apple->x && y == apple->y && z == apple->z)
+		EatApple();
+	if(grow)
+	{
+		//add p to front;
+		return;
+	}
+	Point* p = new Point(x,y,z);
+	if(CheckIfInsidePlayer(p))
+	{
+		delete p;
+		GameOver();
+	}
+	if(x < 0 || y < 0 || z < 0 ||
+		x >= size || y >= size || z >= size)
+	{
+		delete p;
+		GameOver();
+	}
+	//Add p to front and remove last element
+}
+void Snake::GameOver()
+{
+	Reset();
+}
+void Snake::Move()
+{
+	//This Function needs to be called every XXXXX seconds? Not sure where we want to do it
+	//Get From joystick TODO
+	Move(0,0,1);
+}*/
